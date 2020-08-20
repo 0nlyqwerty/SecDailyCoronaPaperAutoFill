015bc8ed670c1c8a4f661a5d7c6029418bf1c50b
beargu2 <powergaeko@gmail.com>
2020-08-21 03:38:37 +0900

    SharedPreference 적용완료

--- app/src/main/java/com/bearguy/secdailycoronapaperautofill/MainActivity.java
+++ app/src/main/java/com/bearguy/secdailycoronapaperautofill/MainActivity.java
@@ -2,6 +2,7 @@ package com.bearguy.secdailycoronapaperautofill;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,17 +12,53 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 	final String TAG = "MAIN_ACTIVITY";
+	private Context mContext;
 	private WebView mWebView;
+
+	private EditText et_userName;
+	private EditText et_empNo;
+	private EditText et_telNumber;
+	private Button btn_set;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mContext = this;
 
 		mWebView = findViewById(R.id.activity_main_webView);
+
+		et_userName = findViewById(R.id.et_name);
+		et_empNo = findViewById(R.id.et_employee_number);
+		et_telNumber = findViewById(R.id.et_phone_number);
+
+		String userName = PreferenceManager.getString(mContext, "USER_NAME");
+		String empNo = PreferenceManager.getString(mContext, "EMPLOYEE_NUMBER");
+		String telNo = PreferenceManager.getString(mContext, "PHONE_NUMBER");
+
+		et_userName.setText(userName);
+		et_empNo.setText(empNo);
+		et_telNumber.setText(telNo);
+
+		btn_set = findViewById(R.id.btn_set_information);
+		btn_set.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Log.d(TAG, "Set button clicked!");
+
+				PreferenceManager.setString(mContext, "USER_NAME", et_userName.getText().toString());
+				PreferenceManager.setString(mContext, "EMPLOYEE_NUMBER", et_empNo.getText().toString());
+				PreferenceManager.setString(mContext, "PHONE_NUMBER", et_telNumber.getText().toString());
+
+				setValues(mWebView);
+			}
+		});
+
+
 		WebSettings webSettings = mWebView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 
@@ -31,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
 				super.onProgressChanged(view, newProgress);
 
 				if (newProgress == 100) {
-					Log.d(TAG,"loading complete !");
+					Log.d(TAG, "loading complete !");
 					setValues(view);
 				}
 			}
@@ -41,25 +78,21 @@ public class MainActivity extends AppCompatActivity {
 		Uri data = intent.getData();
 		Log.d(TAG, "received intent:" + action + "," + data);
 
-		if(data != null){
-			mWebView.loadUrl(data.toString());	// 주어진 Url 을 mWebView 에 load
+		if (data != null) {
+			mWebView.loadUrl(data.toString());    // 주어진 Url 을 mWebView 에 load
 		}
-
-		Button btn = findViewById(R.id.btn_set_information);
-		btn.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Log.d(TAG,"Set button clicked!");
-				setValues(mWebView);
-			}
-		});
 	}
 
-	void setValues(WebView view){
-		Log.d(TAG,"Set Values!");
+	private void setValues(WebView view) {
+		Log.d(TAG, "Set Values!");
 		view.evaluateJavascript("(function() { document.querySelector(\"input[value=SEC]\").click()})()", null);
-		view.evaluateJavascript("(function() { document.getElementById(\"userName\").value=\"1\"})()", null);
-		view.evaluateJavascript("(function() { document.getElementById(\"empNo\").value=\"4\"})()", null);
-		view.evaluateJavascript("(function() { document.getElementById(\"telNumber\").value=\"7\"})()", null);
+
+		String userName = PreferenceManager.getString(mContext, "USER_NAME");
+		String empNo = PreferenceManager.getString(mContext, "EMPLOYEE_NUMBER");
+		String telNo = PreferenceManager.getString(mContext, "PHONE_NUMBER");
+
+		view.evaluateJavascript("(function() { document.getElementById(\"userName\").value=\"" + userName + "\"})()", null);
+		view.evaluateJavascript("(function() { document.getElementById(\"empNo\").value=\"" + empNo + "\"})()", null);
+		view.evaluateJavascript("(function() { document.getElementById(\"telNumber\").value=\"" + telNo + "\"})()", null);
 	}
 }
